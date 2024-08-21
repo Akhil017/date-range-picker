a361bd27728baa00086ba46417f7a4feb2e91268
Akhil017 <akhil.17.akhi@gmail.com>
2024-08-21 23:16:51 +0530

    added calendars popover

--- src/components/range-picker/index.tsx
+++ src/components/range-picker/index.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import RangePickerInput from "./range-picker-input";
+import Calendar from "./calendar";
+import "./range-picker.scss";
 
 export default function RangePicker() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="range-picker">
-      <RangePickerInput />
+      <RangePickerInput onClick={() => setIsOpen(true)} />
+      {isOpen && (
+        <div className="range-picker-popover">
+          <div className="range-picker-popover-header">header</div>
+          <div className="range-picker-popover-calendar">
+            <Calendar />
+          </div>
+          <div className="range-picker-popover-calendar">
+            <Calendar />
+          </div>
+          <div className="range-picker-popover-footer">footer</div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/range-picker/range-picker-input/index.tsx
+++ src/components/range-picker/range-picker-input/index.tsx
@@ -1,11 +1,16 @@
+import * as React from "react";
+
 import { Icons } from "@/components/icons";
 import "./range-picker-input.scss";
 
-export default function RangePickerInput() {
-  return (
-    <div className="range-picker-input">
-      <input />
-      <Icons.calendar />
-    </div>
-  );
-}
+const DateRangeInput = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ ...props }, ref) => (
+  <div className="range-picker-input" {...props} ref={ref}>
+    <input />
+    <Icons.calendar />
+  </div>
+));
+
+export default DateRangeInput;
