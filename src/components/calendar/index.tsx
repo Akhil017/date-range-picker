be9dc78d958ee91a55e5b4ddccedcaaafa67cb91
Akhil017 <akhil.17.akhi@gmail.com>
2024-08-21 22:16:40 +0530

    basic calendar done

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,29 +1,68 @@
 import { useState } from "react";
 import CalendarDays from "./calendar-days";
 import "./calendar.scss";
+import { Icons } from "../icons";
 
 const WEEKDAYS = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
   "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
 ];
 
 export default function Calendar() {
-  const [currentDay] = useState(new Date());
+  const [currentDay, setCurrentDay] = useState(new Date());
+
+  console.log({ currentDay });
+
+  const handlePrevBtnClick = () => {
+    console.log({ currentDay_prev: currentDay });
+    console.log({ getDay_prev: currentDay.getDay() });
+    setCurrentDay(
+      new Date(
+        currentDay.getFullYear(),
+        currentDay.getMonth() - 1,
+        currentDay.getDate()
+      )
+    );
+  };
+
+  const handleNextBtnClick = () => {
+    console.log({ currentDay_next: currentDay });
+    console.log({ getDay_next: currentDay.getDay() });
+    setCurrentDay(
+      new Date(
+        currentDay.getFullYear(),
+        currentDay.getMonth() + 1,
+        currentDay.getDate()
+      )
+    );
+  };
+
+  const monthAndYear = `${
+    MONTHS[currentDay.getMonth()]
+  } ${currentDay.getFullYear()}`;
 
   return (
     <div className="calendar">
-      <div className="calendar-header">Aug 2024</div>
+      <div className="calendar-header">
+        <button className="calendar-month-btn" onClick={handlePrevBtnClick}>
+          <Icons.prev />
+        </button>
+        <p>{monthAndYear}</p>
+        <button className="calendar-month-btn" onClick={handleNextBtnClick}>
+          <Icons.next />
+        </button>
+      </div>
       <div className="calendar-body">
         <div className="calendar-table-header">
           {WEEKDAYS.map((day) => (
---
+++ src/components/icons.tsx
@@ -0,0 +1,38 @@
+type IconProps = React.HTMLAttributes<SVGElement>;
+
+export const Icons = {
+  prev: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-chevron-left"
+      {...props}
+    >
+      <path d="m15 18-6-6 6-6" />
+    </svg>
+  ),
+  next: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-chevron-right"
+      {...props}
+    >
+      <path d="m9 18 6-6-6-6" />
+    </svg>
+  ),
+};
