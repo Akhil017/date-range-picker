1ddb64674de8a73f20c431d598fde437d5bc9030
Akhil017 <akhil.17.akhi@gmail.com>
2024-08-21 21:02:16 +0530

    added variables with scss

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import Calendar from "./components/calendar";
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Calendar />
     </div>
   );
--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,3 +1,33 @@
+import "./calendar.scss";
+
+const WEEKDAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 export default function Calendar() {
-  return <div>Calendar</div>;
+  return (
+    <div className="calendar">
+      <div className="calendar-header">Aug 2024</div>
+      <div className="calendar-body">
+        <div className="calendar-table-header">
+          {WEEKDAYS.map((day) => (
+            <div key={day}>{day}</div>
+          ))}
+        </div>
+        <div className="calendar-table-body"></div>
+      </div>
+    </div>
+  );
 }
